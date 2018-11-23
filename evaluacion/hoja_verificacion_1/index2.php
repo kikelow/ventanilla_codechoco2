296b2f7fba5f70a0327c56849fab6a1bcb86dc3b
David Raga Renteria <davidragarenteria@gmail.com>
2018-11-23 11:30:25 -0500

    jashkda

--- evaluacion/hoja_verificacion_1/index2.php
+++ evaluacion/hoja_verificacion_1/index2.php
@@ -115,7 +115,7 @@
 
                       <div class='input-field col s12 m4 l4'>
 
-                        <select multiple name="medio_verificacion[]" id="medio_verificacion">
+                        <select multiple name="medio_verificacion[]" id="medio_verificacion1">
                           <option disabled>Seleccione...</option>
                           <?php
                            $medio = array();
@@ -123,7 +123,7 @@
                           $r1= mysqli_query($conn,$s1) or die('Error');
                           if(mysqli_num_rows($r1)>0){
                             while($result1=mysqli_fetch_assoc($r1)){
-                              echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
+                              echo"<option value=".$result1['nombre'].">".$result1['nombre' ]."</option>";
                               array_push($medio,$result1['id']);
                             }
                           }
@@ -131,12 +131,7 @@
                           ?>
 
                         </select>
-                        <?php   
-                            for ($i  =0; $i   < sizeof($medio); $i ++) { 
-                              echo "<input type='hidden' name='medio_confirmacion[]' value='$medio[$i]'>";
-                            }
-
-                        ?>
+                       <input type='hidden' name='medio1[]' id='medio1'>
                         
                       </div>
 
@@ -156,7 +151,7 @@
                     
                   <div class="divider"></div>
                   <?php
-
+                  $i = 0;
                   $s="SELECT id,descripcion from pregunta_indicativa where aspecto_id = 2";
                   $r= mysqli_query($conn,$s) or die("Error");
                   if(mysqli_num_rows($r)>0){
@@ -191,7 +186,7 @@
               
                     <div class='input-field col s12 m6 l6'  style='margin-top: 52px'>
 
-                    <select multiple name='medio_verificacion2[]' id='medio_verificacion'>
+                    <select multiple name='medio_verificacion2[]' id='medio_verificacion2".$i."'>
                     <option disabled>Seleccione...</option>";
 
                     $medio1 = array();
@@ -208,9 +203,9 @@
                     echo "
                     </select>";
                        
-                    // for ($i  =0; $i   < sizeof($medio1); $i ++) { 
-                    //   echo "<input type='hidden' name='medio_confirmacion2[]' value='$medio1[$i]'>";
-                    // }
+                   
+                    echo "<input type='hidden' name='medio2[]' id='medio2".$i."' value=''>";
+                 
 
                     echo "
 
@@ -227,105 +222,216 @@
             </li>
 
             <li>
-              <div class="collapsible-header" style="font-weight: bold;"> <i class="material-icons"></i>  Impacto ambiental positivo y contribución a la conservación y preservación de los recursos ecosistemicos </div>
+              <div class="collapsible-header" style="font-weight: bold;"> <i class="material-icons"></i>  Administrativo </div>
               <div class="collapsible-body">
-               <div class="row" style="text-align: center;background-color: #bdbdbd;">Impacto ambiental positivo y contribución a la conservación y preservación de los recursos ecosistemicos </div>
-               <div class="row" style="border: 1px solid;height: auto;display:inline-block; width: 100% ">
-                
-                <div class="divider"></div>
-                <?php 
-                $s="SELECT id,nombre from opciones where codigo LIKE '%IMPACTO_AMBIENTAL%'  order by id ";
-                $r= mysqli_query($conn,$s) or die("Error");
-                if(mysqli_num_rows($r)>0){
-                  while($rw=mysqli_fetch_assoc($r)){
+               <div class="row" style="text-align: center;background-color: #bdbdbd;">Administrativo </div> <div class="divider"></div>
+                  <?php
+                  $i = 0;
+                  $s="SELECT id,descripcion from pregunta_indicativa where aspecto_id = 3";
+                  $r= mysqli_query($conn,$s) or die("Error");
+                  if(mysqli_num_rows($r)>0){
+                    while($rw=mysqli_fetch_assoc($r)){
 
-                   $i++;
-                   echo"
-                   <div class='row'>
-                   <div class='input-field col s12 m6 l6'>
-                   <input type='hidden' name='opcion[]' value='$rw[id]' />
-                   <label for=''>$rw[nombre]</label>
-                   </div>
-                   <div class='input-field col s12 m2 l2' style='margin-top: 52px'>
-                   <select name='verifica1_si_no[]' id='verifica1_si_no".$i."'>";
-                   $s1="select id,nombre from si_no_noaplica order by id desc";
-                   $r1= mysqli_query($conn,$s1) or die('Error');
-                   if(mysqli_num_rows($r1)>0){
-                    while($result1=mysqli_fetch_assoc($r1)){
-                      echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
+                     $i++;
+                     echo"
+                     <div class='row'>
+                     <div class='input-field col s12 m6 l6' style='margin-top: 50px'>
+                     <input type='hidden' name='preguntas3[]' value='$rw[id]' />
+                     <p style='text-align:justify'>$rw[descripcion]</p>
+                     </div>
+                     <div class='input-field col s12 m6 l6' style='margin-top: 52px'>
+                     <select name='verifica1_si_no3[]' id='verifica1_si_no3".$i."'>";
+                     $s1="select id,nombre from si_no order by id desc";
+                     $r1= mysqli_query($conn,$s1) or die('Error');
+                     if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
+                      }
                     }
-                  }
-                  echo"
-                  </select>
-                  <label for=''>Si/ No/ No Aplica</label>
-                  </div>
-                  <div class='input-field col s12 m4 l4'>
-                  <textarea id='verificacion1_obs".$i."' name='verificacion1_obs[]' class='materialize-textarea'></textarea>
-                  <label for=''>Observaciones</label>
-                  </div>
-                  </div>
-                  <div class='divider'></div>";
+                    echo"
+                    </select>
+                    <label for=''>Si/ No/ No Aplica</label>
+                    </div>
 
-                }         
-              }
-              ?>
+
+                    <div class='input-field col s12 m6 l6'>
+                    <textarea id='descripcion3".$i."' name='descripcion3[]' class='materialize-textarea'></textarea>
+                    <label for=''>Descripción</label>
+                    </div>
               
-            </div>
+                    <div class='input-field col s12 m6 l6'  style='margin-top: 52px'>
+
+                    <select multiple name='medio_verificacion3[]' id='medio_verificacion3".$i."'>
+                    <option disabled>Seleccione...</option>";
+
+                    $medio1 = array();
+                    $s1="SELECT id,nombre from medio order by id asc ";
+                    $r1= mysqli_query($conn,$s1) or die('Error');
+                    if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['nombre'].">".$result1['nombre' ]."</option>";
+                        // array_push($medio1,$result1['id']);
+                      }
+                    }
+
+                   
+                    echo "
+                    </select>";
+                       
+                   
+                    echo "<input type='hidden' name='medio3[]' id='medio3".$i."' value=''>";
+                 
+
+                    echo "
+
+                    </div>
+                    
+                    </div>
+                    <div class='divider'></div>";
+
+                  }         
+                }
+                ?>
+           
           </li>
 
           <li>
-            <div class="collapsible-header" style="font-weight: bold;"> <i class="material-icons"></i> Impacto social positivo </div>
+            <div class="collapsible-header" style="font-weight: bold;"> <i class="material-icons"></i>Ambiental</div>
             <div class="collapsible-body">
-             <div class="row" style="text-align: center;background-color: #bdbdbd;">Impacto social positivo</div>
-             <div class="row" style="border: 1px solid;height: auto;display:inline-block; width: 100% ">
-              
+             <div class="row" style="text-align: center;background-color: #bdbdbd;">Ambiental</div>
               <div class="divider"></div>
-              <?php 
-              $s="SELECT id,nombre from opciones where codigo LIKE '%IMPACTO_SOCIAL%'  order by id ";
-              $r= mysqli_query($conn,$s) or die("Error");
-              if(mysqli_num_rows($r)>0){
-                while($rw=mysqli_fetch_assoc($r)){
+              <?php                  $i = 0;
+                  $s="SELECT id,descripcion from pregunta_indicativa where aspecto_id = 4 ";
+                  $r= mysqli_query($conn,$s) or die("Error");
+                  if(mysqli_num_rows($r)>0){
+                    while($rw=mysqli_fetch_assoc($r)){
+                     $i++;
+                      if($rw['id'] != 26){
+                     echo"
+                     <div class='row'>
+                     <div class='input-field col s12 m6 l6' >
+                     <input type='hidden' name='preguntas4[]' value='$rw[id]' />
+                     <p style='text-align:justify'>$rw[descripcion]</p>
+                     </div>
+                     <div class='input-field col s12 m3 l3' style='margin-top: 52px'>
+                     <select name='verifica1_si_no4[]' id='verifica1_si_no4".$i."'>";
+                     $s1="select id,nombre from si_no order by id desc";
+                     $r1= mysqli_query($conn,$s1) or die('Error');
+                     if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
+                      }
+                    }
+                    echo"
+                    </select>
+                    <label for=''>Si/ No/ No Aplica</label>
+                    </div>
 
-                 $i++;
-                 echo"
-                 <div class='row'>
-                 <div class='input-field col s12 m6 l6'>
-                 <input type='hidden' name='opcion[]' value='$rw[id]' />
-                 <label for=''>$rw[nombre]</label>
-                 </div>
-                 <div class='input-field col s12 m2 l2' style='margin-top: 52px'>
-                 <select name='verifica1_si_no[]' id='verifica1_si_no".$i."'>";
-                 $s1="select id,nombre from si_no_noaplica order by id desc";
-                 $r1= mysqli_query($conn,$s1) or die('Error');
-                 if(mysqli_num_rows($r1)>0){
-                  while($result1=mysqli_fetch_assoc($r1)){
-                    echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
-                  }
+                     <div class='input-field col s12 m3 l3' style='margin-top: 52px'>
+                     <select name='verifica1_cumple4[]' id='verifica1_cumple4".$i."'>";
+                     $s1="select id,nombre from si_no order by id desc";
+                     $r1= mysqli_query($conn,$s1) or die('Error');
+                     if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
+                      }
+                    }
+                    echo"
+                    </select>
+                    <label for=''>Cumple con el requerimiento</label>
+                    </div>
+
+                    <div class='input-field col s12 m6 l6'>
+                    <textarea id='descripcion4".$i."' name='descripcion4[]' class='materialize-textarea'></textarea>
+                    <label for=''>Descripción</label>
+                    </div>
+              
+                    <div class='input-field col s12 m6 l6'  style='margin-top: 52px'>
+
+                    <select multiple name='medio_verificacion4[]' id='medio_verificacion4".$i."'>
+                    <option disabled>Seleccione...</option>";
+
+                    $medio1 = array();
+                    $s1="SELECT id,nombre from medio order by id asc ";
+                    $r1= mysqli_query($conn,$s1) or die('Error');
+                    if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['nombre'].">".$result1['nombre' ]."</option>";
+          
+                      }
+                    }
+
+                   
+                    echo "
+                    </select>
+                   <input type='hidden' name='medio4[]' id='medio4".$i."' value=''>
+                    </div>
+                    
+                    </div>
+                    <div class='divider'></div>";
+
+                  }else if ($rw['id'] == 26) {
+                     echo"
+                     <div class='row'>
+                     <div class='input-field col s12 m6 l6' >
+                     <input type='hidden' name='preguntas4[]' value='$rw[id]' />
+                     <p style='text-align:justify'>$rw[descripcion]</p>
+                     </div>
+                     <div class='input-field col s12 m6 l6' style='margin-top: 52px'>
+                     <select name='verifica1_si_no4[]' id='verifica1_si_no4".$i."'>";
+                     $s1="select id,nombre from si_no order by id desc";
+                     $r1= mysqli_query($conn,$s1) or die('Error');
+                     if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['id'].">".$result1['nombre' ]."</option>";
+                      }
+                    }
+                    echo"
+                    </select>
+                    <label for=''>Si/ No/ No Aplica</label>
+                    </div>
+
+                    <div class='input-field col s12 m6 l6'>
+                    <textarea id='descripcion4".$i."' name='descripcion4[]' class='materialize-textarea'></textarea>
+                    <label for=''>Descripción</label>
+                    </div>
+              
+                    <div class='input-field col s12 m6 l6'  style='margin-top: 52px'>
+
+                    <select multiple name='medio_verificacion4[]' id='medio_verificacion4".$i."'>
+                    <option disabled>Seleccione...</option>";
+
+                    $medio1 = array();
+                    $s1="SELECT id,nombre from medio order by id asc ";
+                    $r1= mysqli_query($conn,$s1) or die('Error');
+                    if(mysqli_num_rows($r1)>0){
+                      while($result1=mysqli_fetch_assoc($r1)){
+                        echo"<option value=".$result1['nombre'].">".$result1['nombre' ]."</option>";
+          
+                      }
+                    }
+
+                   
+                    echo "
+                    </select>
+                   <input type='hidden' name='medio4[]' id='medio4".$i."' value=''>
+                    </div>
+                    
+                    </div>
+                    <div class='divider'></div>";
+                  }       
                 }
-                echo"
-                </select>
-                <label for=''>Si/ No/ No Aplica</label>
-                </div>
-                <div class='input-field col s12 m4 l4'>
-                <textarea id='verificacion1_obs".$i."' name='verificacion1_obs[]' class='materialize-textarea'></textarea>
-                <label for=''>Observaciones</label>
-                </div>
-                </div>
-                <div class='divider'></div>";
-
-              }         
-            }
-            ?>
+              }
+                ?>
             
           </div>
         </li>
 
         <li>
-          <div class="collapsible-header" style="font-weight: bold;"> <i class="material-icons"></i> Sustancias o materiales peligrosos</div>
+          <div class="collapsible-header" style="font-weight: bold;"> <i class="material-icons"></i>Social</div>
           <div class="collapsible-body">
-           <div class="row" style="text-align: center;background-color: #bdbdbd;">Sustancias o materiales peligrosos</div>
-           <div class="row" style="border: 1px solid;height: auto;display:inline-block; width: 100% ">
-            
+           <div class="row" style="text-align: center;background-color: #bdbdbd;">Social</div>
+          
             <div class="divider"></div>
             <?php 
             $s="SELECT id,nombre from opciones where codigo LIKE '%MATERIAL_PELIGROSO%'  order by id ";
@@ -363,8 +469,7 @@
             }         
           }
           ?>
-          
-        </div>
+      
       </li>
     </ul>
     
